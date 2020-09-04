98c9b29e017fc213a07ce057c562a0055e1ccb51
AlexanderKurtsevich <alexander.kurtsevich@gmail.com>
2020-09-04 10:00:20 +0300

    feat: add redux-dev-tools to project

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers/rootReducer';
 import createSagaMiddleware from 'redux-saga';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const sagaMiddleware = createSagaMiddleware()
 const store = createStore(
     rootReducer,
-    applyMiddleware(sagaMiddleware)
+    composeEnhancers(applyMiddleware(sagaMiddleware))
 );
 
 export default store;
